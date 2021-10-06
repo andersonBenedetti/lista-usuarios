a09f9f816c7c3de40aba464de54b044aa9c98263
Anderson <andersonjcbenedetti@gmail.com>
2021-10-06 01:18:07 -0300

    Primeiro commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import api from './services/api';
+import './styles.css';
 
-function App() {
+export default function App() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadUsuarios() {
+      const response = await api.get('users');
+      setUsuarios(response.data);
+      setLoading(false);
+    }
+    loadUsuarios();
+  }, []);
+
+  function deleteUsuario(id) {
+    const novaLista = usuarios.filter((i) => i.id !== id);
+    setUsuarios(novaLista);
+    window.alert('Usuário será excluído!');
+  }
+
+  if (loading) {
+    return (
+      <div className="pag-loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Lista de usuários</h1>
       </header>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Empresa</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {usuarios.map((i) => {
+            return (
+              <tr key={i.id}>
+                <td> {i.name} </td>
+                <td> {i.email} </td>
+                <td> {i.company.name} </td>
+
+                <td>
+                  <button onClick={() => deleteUsuario(i.id)}> Excluir </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
